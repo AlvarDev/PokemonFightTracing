82d8bb96c2442920b18d346412e8c41533b83965
AlvarDev <alvardev.lp@gmail.com>
2021-08-04 03:00:24 -0300

    Add logic for fight winner

--- fight/go.mod
+++ fight/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	contrib.go.opencensus.io/exporter/stackdriver v0.13.8
 	github.com/gorilla/mux v1.8.0
+	github.com/mtslzr/pokeapi-go v1.4.0 // indirect
 	github.com/rs/zerolog v1.23.0
 	github.com/yfuruyama/crzerolog v0.3.0
 	go.opencensus.io v0.23.0
--- fight/main.go
+++ fight/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
+	"regexp"
 
 	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
 	"github.com/gorilla/mux"
+	"github.com/mtslzr/pokeapi-go"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/yfuruyama/crzerolog"
@@ -15,7 +18,7 @@ import (
 
 func main() {
 	mux := mux.NewRouter()
-	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/", rootHandler).Methods("POST")
 
 	rootLogger := zerolog.New(os.Stdout)
 	middleware := crzerolog.InjectLogger(&rootLogger)
@@ -33,6 +36,35 @@ func main() {
 	}
 }
 
+// Request struct handler
+type PokemonsFight struct {
+	PokemonA struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Photo string `json:"photo"`
+		Types []struct {
+			Slot int `json:"slot"`
+			Type struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"type"`
+		} `json:"types"`
+	} `json:"pokemonA"`
+
+	PokemonB struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Photo string `json:"photo"`
+		Types []struct {
+			Slot int `json:"slot"`
+			Type struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"type"`
+		} `json:"types"`
+	} `json:"pokemonB"`
+}
+
 // Response definition for response API
 type Response struct {
 	Message string `json:"message"`
@@ -40,13 +72,78 @@ type Response struct {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 
-	// re := regexp.MustCompile("/type/([0-9]+)*")
+	var pf PokemonsFight
+	err := json.NewDecoder(r.Body).Decode(&pf)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	re := regexp.MustCompile("/type/([0-9]+)*")
+
+	matchA := re.FindStringSubmatch(pf.PokemonA.Types[0].Type.URL)
+	matchB := re.FindStringSubmatch(pf.PokemonB.Types[0].Type.URL)
+
+	typeA, errA := pokeapi.Type(matchA[1])
+	typeB, errB := pokeapi.Type(matchB[1])
+
+	if errA != nil {
+		log.Ctx(r.Context()).Error().Err(errA)
+		http.Error(w, errA.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if errB != nil {
+		log.Ctx(r.Context()).Error().Err(errB)
+		http.Error(w, errB.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	damagedTypeA := 0
+	damagedTypeB := 0
+
+	for _, v := range typeA.DamageRelations.DoubleDamageFrom {
+		if v.Name == pf.PokemonB.Types[0].Type.Name {
+			damagedTypeA += 2
+		}
+
+		fmt.Print("\t" + v.Name + ": ")
+		fmt.Println(damagedTypeA)
+	}
+
+	fmt.Print("Total damage taken: ")
+	fmt.Println(damagedTypeA)
+	fmt.Println("------------")
+
+	fmt.Println("Score B: ")
+
+	for _, v := range typeB.DamageRelations.DoubleDamageFrom {
+		if v.Name == pf.PokemonA.Types[0].Type.Name {
+			damagedTypeB += 2
+		}
+		fmt.Print("\t" + v.Name + ": ")
+		fmt.Println(damagedTypeB)
+	}
+
+	fmt.Print("Total damage taken: ")
+	fmt.Println(damagedTypeB)
+
+	if damagedTypeA < damagedTypeB {
+		fmt.Println("A won")
+	} else if damagedTypeB < damagedTypeA {
+		fmt.Println("B won")
+	} else {
+		fmt.Println("Draw!")
+	}
+
 	response := Response{}
 	response.Message = "Hello world!"
 	responseJSON, err := json.Marshal(response)
 
 	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to parse json")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
